811706a6f564c98aa72b0ef17988db54f0cc6061
Tokyo42 <125569485+Tokyo42@users.noreply.github.com>
2023-02-22 13:25:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -124,7 +124,7 @@ async def next_page(bot, query):
     # How to Download button
    
     btn.append(
-    [InlineKeyboardButton("🍃 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍃", url="https://t.me/How_to_Download_TnLinkk/7"),]
+    [InlineKeyboardButton("🤔𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗🤔", url="https://t.me/How_to_Download_TnLinkk/7"),]
 )
     if n_offset == 0:
         btn.append(
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
     )
     
     btn.append(
-    [InlineKeyboardButton("🍃 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🍃", url="https://t.me/How_to_Download_TnLinkk/7"),]
+    [InlineKeyboardButton("🤔𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗🤔", url="https://t.me/How_to_Download_TnLinkk/7"),]
 )
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
