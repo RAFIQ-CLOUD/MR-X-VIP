7705697e8e319e33759efe359a684633edfb6837
MS-BOTZZZ <125134635+MS-BOTZZZ@users.noreply.github.com>
2023-02-23 22:15:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Vip2
 fi
-cd /Filterbot1
+cd /Vip2
 pip3 install -U -r requirements.txt
 echo "🍃Starting Bot🍃...."
 python3 bot.py
